c90b786778ed7a82dbdf6be1d9c97caac05ebb76
Jeroen Engels <jfm.engels@gmail.com>
2016-07-06 16:14:12 +0200

    Replace mocha-only by mocha and add new rules

--- config/mocha.js
+++ config/mocha.js
@@ -5,9 +5,23 @@ module.exports = {
         mocha: true
     },
     plugins: [
-        'mocha-only'
+        'mocha'
     ],
     rules: {
-        'mocha-only/mocha-only': 'error'
+        'mocha/handle-done-callback': 'error',
+        'mocha/no-exclusive-tests': 'error',
+        'mocha/no-global-tests': 'error',
+        'mocha/no-hooks': 'off',
+        "mocha/no-hooks-for-single-case": ["error", {"allow": ["after"]}],
+        'mocha/no-identical-title': 'error',
+        'mocha/no-mocha-arrows': 'error',
+        'mocha/no-pending-tests': 'error',
+        'mocha/no-return-and-callback': 'error',
+        'mocha/no-sibling-hooks': 'error',
+        'mocha/no-skipped-tests': 'error',
+        'mocha/no-synchronous-tests': 'off',
+        'mocha/no-top-level-hooks': 'error',
+        'mocha/valid-suite-description': 'off',
+        'mocha/valid-test-description': 'off'
     }
 };
