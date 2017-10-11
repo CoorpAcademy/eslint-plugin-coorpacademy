3feb13c3a2ae8a7a2e027d94be4442f46e127537
Jeroen Engels <jfm.engels@gmail.com>
2017-10-11 12:26:36 +0200

    Add nyc

--- index.js
+++ index.js
@@ -4,13 +4,5 @@ const requireIndex = require('requireindex');
 
 module.exports = {
   rules: requireIndex(__dirname + '/rules'),
-  configs: {
-    ava: require('./config/ava'),
-    core: require('./config/core'),
-    es20XX: require('./config/es20XX'),
-    react: require('./config/react'),
-    'lodash-fp': require('./config/lodash-fp'),
-    mocha: require('./config/mocha'),
-    prettier: require('./config/prettier')
-  }
+  configs: requireIndex(__dirname + '/config')
 };
---
+++ test/index.js
@@ -0,0 +1,13 @@
+import test from 'ava';
+import m from '..';
+
+test('index should contain all configurations', t => {
+  t.deepEqual(
+    Object.keys(m.configs).sort(),
+    ['ava', 'core', 'es20XX', 'flowtype', 'lodash-fp', 'mocha', 'prettier', 'react'].sort()
+  );
+});
+
+test('index should contain all rules', t => {
+  t.deepEqual(Object.keys(m.rules).sort(), ['no-dangerous-logs', 'use-expect'].sort());
+});
