fbbda5c066d37644a18fb5630b8396d252e1204b
Adriean Khisbe <adriean.khisbe@live.fr>
2019-10-23 11:01:26 +0200

    Add use-expect replacement with chai-expect eslint plugin :battery:

--- config/mocha.js
+++ config/mocha.js
@@ -2,7 +2,7 @@ module.exports = {
   env: {
     mocha: true
   },
-  plugins: ['mocha'],
+  plugins: ['mocha', 'chai-expect'],
   rules: {
     'mocha/handle-done-callback': 'error',
     'mocha/max-top-level-suites': 'off',
@@ -21,6 +21,10 @@ module.exports = {
     'mocha/no-synchronous-tests': 'off',
     'mocha/no-top-level-hooks': 'error',
     'mocha/valid-suite-description': 'off',
-    'mocha/valid-test-description': 'off'
+    'mocha/valid-test-description': 'off',
+
+    'chai-expect/no-inner-compare': 'error',
+    'chai-expect/missing-assertion': 'error',
+    'chai-expect/terminating-properties': 'error'
   }
 };
